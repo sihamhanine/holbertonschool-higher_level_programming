e3cd3a909044985d1b5e3af367e6a9baec1a644d
sihamelani <siham.elani17@gmail.com>
2024-05-08 16:55:22 +0000

    Correction pycodestyle

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,13 +4,10 @@ number = random.randint(-10000, 10000)
 lastd = abs(number) % 10
 if number < 0:
     lastd *= -1
-#imprimer le last digit
-print("Last digit of", number, "is", lastd, end=" ")
+    print("Last digit of", number, "is", lastd, end=" ")
 if lastd > 5:
     print("and is greater than 5")
 elif lastd == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
-    
-    
