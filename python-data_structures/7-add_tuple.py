8c0282925cf5b2eecc617b88f6dddfae36746d3d
sihamelani <siham.elani17@gmail.com>
2024-05-14 07:48:55 +0000

    Task 7 correction pycodestyle

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,8 +2,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a + (0, 0)
         tuple_b = tuple_b + (0, 0)
-        
-        tuple1 = tuple_a[0] + tuple_b[0]
-        tuple2 = tuple_a[1] + tuple_b[1]
-        
-        return (tuple1, tuple2)
+        sum_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        return sum_tuple
