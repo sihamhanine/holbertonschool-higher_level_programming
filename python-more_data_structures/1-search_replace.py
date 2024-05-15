67d04555e23e2302c9ee9bef5b43f92e85fec032
sihamelani <siham.elani17@gmail.com>
2024-05-15 09:34:20 +0000

    Task 1 Write a function that replaces all occurrences of an element by another in a new list.

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    elem = my_list[0]
     for elem in my_list:
         if elem == search:
             new_list.append(replace)
