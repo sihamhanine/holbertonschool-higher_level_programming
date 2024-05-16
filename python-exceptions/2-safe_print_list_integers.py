e0a929b0016d6560b9268c432f72916c512f2a67
sihamelani <siham.elani17@gmail.com>
2024-05-16 16:11:42 +0200

    Task 3 Write a function that divides 2 integers and prints the result. and correction task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     printed_integers = 0
 
@@ -9,5 +10,5 @@ def safe_print_list_integers(my_list=[], x=0):
         except TypeError:
             continue
     print()
-    
+
     return printed_integers
