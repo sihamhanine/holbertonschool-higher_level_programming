ecd8b246994e8a3ffa00e350a5fa98c8e0e6667d
sihamelani <siham.elani17@gmail.com>
2024-05-17 13:00:28 +0000

    Task 4 correction

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -19,5 +19,6 @@ def list_division(my_list_1, my_list_2, list_length):
                 new_list.append(0)
     except IndexError:
         print("out of range")
+        new_list.append(0)
     finally:
         return new_list
