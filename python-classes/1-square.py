f7be4dc2fd22c8218e5549d615e3b03b5de7292c
sihamelani <siham.elani17@gmail.com>
2024-05-17 15:33:55 +0000

    Task 0 et 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
 """Square Class
-
 A Square Class
-
 """
-
 class Square:
     """Define square."""
-    def __init__(self, size):
-        
+    def __init__(self, size):     
         """__init__
-
         Method initializes the size value
         of the square
-
         Attributes:
             - size (int): 
         The size of the square.
-
         """
         self.__size = size
