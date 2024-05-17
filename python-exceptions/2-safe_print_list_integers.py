3a6327cae71bec933edda0bea6f80de589fa4e2f
sihamelani <siham.elani17@gmail.com>
2024-05-17 12:19:40 +0000

    Task 2 correction

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     printed_integers = 0
-
     for i in range(x):
         try:
             if type(my_list[i]) is int and printed_integers != x:
@@ -10,5 +9,4 @@ def safe_print_list_integers(my_list=[], x=0):
         except TypeError:
             continue
     print()
-
     return printed_integers
