247bef1c9472d30542dabe6caee7c858937cce1b
sihamelani <siham.elani17@gmail.com>
2024-05-17 21:00:03 +0200

    Task3

---
+++ python-classes/3-square.py
@@ -0,0 +1,42 @@
+class Square:
+    """
+    A class that defines a square by its size.
+    
+    Attributes:
+    ----------
+    __size : int
+        The size of the square's sides.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance with the specified size.
+        
+        Parameters:
+        ----------
+        size : int, optional
+            The size of the square's sides (default is 0).
+        
+        Raises:
+        ------
+        TypeError:
+            If size is not an integer.
+        ValueError:
+            If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the current square area.
+        
+        Returns:
+        -------
+        int
+            The area of the square.
+        """
+        return self.__size * self.__size
