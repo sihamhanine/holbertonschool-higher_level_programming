43738fc7cae56aa7dcf4117efe774b22b8c11d88
sihamelani <siham.elani17@gmail.com>
2024-05-21 12:31:24 +0200

    Task 1 Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -26,8 +26,8 @@ class Rectangle:
         height : int
             The height of the rectangle's sides.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
