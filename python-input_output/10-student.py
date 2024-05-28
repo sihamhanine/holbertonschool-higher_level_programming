4ea1e3fc93ae1d336ac92a141e64d44685d25b60
sihamelani <siham.elani17@gmail.com>
2024-05-28 12:37:04 +0200

    Task 10 et 11

---
+++ python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defines module student"""
+
+
+class Student:
+    """Defines a class"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Method that instantiate the attribute of a class
+
+        Args:
+            first_name: the first name
+            last_name: the last name
+            age: the age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Method that retrieves a dictionary representation of a Student
+        """
+        if attrs is None:
+            self.__dict__
+        if isinstance(attrs, list):
+            filtered_attributes = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    filtered_attributes[key] = value
+            return filtered_attributes
+        
+        return self.__dict__
---
+++ python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Defines module student"""
+
+
+class Student:
+    """Defines a class"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Method that instantiate the attribute of a class
+
+        Args:
+            first_name: the first name
+            last_name: the last name
+            age: the age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Method that retrieves a dictionary representation of a Student
+        """
+        if attrs is None:
+            self.__dict__
+        if isinstance(attrs, list):
+            filtered_attributes = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    filtered_attributes[key] = value
+            return filtered_attributes
+        
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Method that replaces all attributes of the Student instance
+
+        Args:
+            json: the dictionnary json string
+        """
+        for i in json:
+            if i in self.__dict__.keys():
+                self.__dict__[i] = json[i]
