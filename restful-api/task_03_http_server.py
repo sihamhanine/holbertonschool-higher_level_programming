b7013c8b8cfde0999ee174bf32ea1501b836429c
sihamelani <siham.elani17@gmail.com>
2024-06-01 15:15:00 +0000

    Task 03 correction

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -7,7 +7,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     """
     Defines a class SimpleHTTPRequestHandler
     """
-    def do_GET():
+    def do_GET(self):
         """
         Method that handle GET requests
         """
@@ -36,7 +36,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
-    server_address = ('', port)
+    server_address = ('0.0.0.0', port)
     httpd = server_class(server_address, handler_class)
     print(f"Starting httpd server on port {port}")
     httpd.serve_forever()
