eb9440d43e77f72eee08f7337b70ef3a93bde52b
sihamelani <siham.elani17@gmail.com>
2024-06-03 09:00:31 +0000

    Task 5

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -14,65 +14,35 @@ app = Flask(__name__)
 
 users = {}
 
-
 @app.route('/')
 def home():
-    """
-    Welcome message endpoint
-
-    Returns:
-        str: A welcome message.
-    """
     return "Welcome to the Flask API!"
 
-
-@app.route('/data')
-def get_usernames():
-    """
-    Endpoint to return list of usernames
-
-    Returns:
-        JSON: A list of usernames stored in the 'users' dictionary.
-    """
+@app.route('/data', methods=['GET'])
+def get_all_usernames():
     return jsonify(list(users.keys()))
 
-
-@app.route('/status')
+@app.route('/status', methods=['GET'])
 def status():
-    """
-    Endpoint to return status
-
-    Returns:
-        str: "OK" indicating that the API is running.
-    """
     return "OK"
 
-
-@app.route('/users/<username>')
+@app.route('/users/<username>', methods=['GET'])
 def get_user(username):
-    """
-    Endpoint to return user details by username
-
-    Args:
-        username (str): The username to retrieve.
-
-    Returns:
-        JSON: User details if the username exists, otherwise an error message.
-    """
-    if username in users:
-        return jsonify(users[username])
+    user = users.get(username)
+    if user:
+        return jsonify(user)
     else:
-        return jsonify({"error": "User not found"})
-
+        return jsonify({"error": "User not found"}), 404
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.json
     username = data.get('username')
+
+    if username in users:
+        return jsonify({"error": "Username already exists"}), 400
     if not username:
         return jsonify({"error": "Username is required"}), 400
-    elif username in users:
-        return jsonify({"error": "Username already exists"}), 400
     
     users[username] = {
         "username": username,
@@ -83,5 +53,5 @@ def add_user():
     return jsonify({"message": "User added", "user": users[username]}), 201
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
