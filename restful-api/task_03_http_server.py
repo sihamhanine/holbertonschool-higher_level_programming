5ceaec1ccefdcad12e36fa02cd7e2075a44baf3c
sihamhanine <159253357+sihamhanine@users.noreply.github.com>
2024-06-06 10:11:16 +0200

    Update task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -26,7 +26,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             data = {
-
+                "name": "John",
+                "age": 30,
+                "city": "New York"
             }
             self.wfile.write(json.dumps(data).encode("utf-8"))
         elif self.path == '/info':
