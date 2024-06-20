6767ffda0fb2480323a3ace80b64cb6828aa4256
sihamelani <siham.elani17@hotmail.com>
2024-06-20 12:32:20 +0200

    Task 0 correction pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
     Access to database and get the states
     from database
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+    passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
