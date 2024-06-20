373e478916ef735d0a4855baabd1b851aaa93df2
sihamelani <siham.elani17@hotmail.com>
2024-06-20 12:34:09 +0200

    Task 0 correction pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     from database
     """
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-    passwd=argv[2], db=argv[3])
+        passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
