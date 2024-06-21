4c52e72a7e3b639fba0f97b93e92bb6ea2351748
sihamelani <siham.elani17@hotmail.com>
2024-06-21 05:05:33 +0200

    Task 6 correction pycodestyle

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -17,12 +17,14 @@ class State(Base):
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
+
 
 if __name__ == "__main__":
     host = "localhost"
-    #create an engine
-    engine = create_engine(f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@{host}:3306/{sys.argv[3]}", pool_pre_ping=True)
+    # create engine
+    engine = create_engine(f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@ \
+    {host}:3306/{sys.argv[3]}", pool_pre_ping=True)
 
     # Create all tables in the engine
     Base.metadata.create_all(engine)
